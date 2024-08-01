0f564c2d3010b5150345cd303b96f22eb06d1ecf
IlyaPryzhykau <tuarone@gmail.com>
2024-08-01 02:24:54 +0000

    Add configurations and update requirements

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,13 +1,16 @@
+from decouple import Config, RepositoryEnv
 import os
 from pathlib import Path
 
+env = Config(RepositoryEnv('.env'))
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
+SECRET_KEY = env('SECRET_KEY')
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['89.169.169.14', '127.0.0.1', 'localhost', 'richi-test.zapto.org']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -90,10 +93,11 @@ USE_L10N = True
 USE_TZ = True
 
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static_backend/'
+STATIC_ROOT= BASE_DIR / 'static_backend'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = '/var/www/kittygram/media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
